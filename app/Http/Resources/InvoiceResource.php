1d66f3df5ae529b53d69ce627b7765f810d9ced2
VKORONA <s.vibert@yahoo.fr>
2026-07-18 16:03:48 +0200

    fix(invoices): serialize company only when explicitly loaded

--- app/Http/Resources/InvoiceResource.php
+++ app/Http/Resources/InvoiceResource.php
@@ -62,10 +62,7 @@ class InvoiceResource extends JsonResource
             'creator' => $this->when($this->creator()->exists(), fn () => new UserResource($this->creator)),
             'taxes' => $this->when($this->taxes()->exists(), fn () => TaxResource::collection($this->taxes)),
             'fields' => $this->when($this->fields()->exists(), fn () => CustomFieldValueResource::collection($this->fields)),
-            'company' => $this->when(
-                ! $request->isMethod('POST') && $this->company()->exists(),
-                fn () => new CompanyResource($this->company)
-            ),
+            'company' => $this->whenLoaded('company', fn () => new CompanyResource($this->company)),
             'currency' => $this->when($this->currency()->exists(), fn () => new CurrencyResource($this->currency)),
         ];
     }
