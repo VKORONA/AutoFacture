4e54ffe0fb532497f2018fb3a3e756bcca36418c
VKORONA <s.vibert@yahoo.fr>
2026-07-18 09:04:48 +0200

    refactor(quality): typer les gardes d immutabilité comptable

--- app/Providers/InvoiceIntegrityServiceProvider.php
+++ app/Providers/InvoiceIntegrityServiceProvider.php
@@ -34,20 +34,28 @@ class InvoiceIntegrityServiceProvider extends ServiceProvider
 
             if ($forbidden !== []) {
                 throw new FinalizedInvoiceMutationException(
-                    'La facture est scellée. Champs interdits : '.implode(', ', $forbidden).'. Créez un avoir.'
+                    'La facture est scellée. Champs interdits : '.implode(', ', $forbidden).'. Créez un avoir.',
                 );
             }
         });
 
         Invoice::deleting(function (Invoice $invoice): void {
             if ($invoice->finalized_at) {
-                throw new FinalizedInvoiceMutationException();
+                throw new FinalizedInvoiceMutationException;
             }
         });
 
-        CreditNote::updating(fn () => throw new DomainException('Un avoir émis est immuable.'));
-        CreditNote::deleting(fn () => throw new DomainException('Un avoir émis ne peut pas être supprimé.'));
-        CreditNoteItem::updating(fn () => throw new DomainException('Une ligne d’avoir émise est immuable.'));
-        CreditNoteItem::deleting(fn () => throw new DomainException('Une ligne d’avoir émise ne peut pas être supprimée.'));
+        CreditNote::updating(function (): never {
+            throw new DomainException('Un avoir émis est immuable.');
+        });
+        CreditNote::deleting(function (): never {
+            throw new DomainException('Un avoir émis ne peut pas être supprimé.');
+        });
+        CreditNoteItem::updating(function (): never {
+            throw new DomainException('Une ligne d’avoir émise est immuable.');
+        });
+        CreditNoteItem::deleting(function (): never {
+            throw new DomainException('Une ligne d’avoir émise ne peut pas être supprimée.');
+        });
     }
 }
