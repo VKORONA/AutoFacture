1073086f365520ad34f5879f59724e13a058a104
VKORONA <s.vibert@yahoo.fr>
2026-07-19 21:14:08 +0200

    feat(einvoicing): expose onboarding endpoints

--- routes/autofacture.php
+++ routes/autofacture.php
@@ -1,6 +1,7 @@
 <?php
 
 use Crater\Http\Controllers\V1\Admin\CreditNote\CreditNotesController;
+use Crater\Http\Controllers\V1\Admin\ElectronicInvoicing\ElectronicInvoiceConnectionController;
 use Crater\Http\Controllers\V1\Admin\Invoice\FinalizeInvoiceController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,17 @@ Route::prefix('v1')
 
         Route::get('/credit-notes/{creditNote}', [CreditNotesController::class, 'show'])
             ->name('credit-notes.show');
+
+        Route::prefix('electronic-invoicing')->group(function (): void {
+            Route::get('/connection', [ElectronicInvoiceConnectionController::class, 'show'])
+                ->name('electronic-invoicing.connection.show');
+            Route::post('/connection/progress', [ElectronicInvoiceConnectionController::class, 'updateProgress'])
+                ->name('electronic-invoicing.connection.progress');
+            Route::put('/connection/credentials', [ElectronicInvoiceConnectionController::class, 'storeCredentials'])
+                ->name('electronic-invoicing.connection.credentials');
+            Route::post('/connection/test', [ElectronicInvoiceConnectionController::class, 'test'])
+                ->name('electronic-invoicing.connection.test');
+            Route::delete('/connection', [ElectronicInvoiceConnectionController::class, 'disconnect'])
+                ->name('electronic-invoicing.connection.disconnect');
+        });
     });
