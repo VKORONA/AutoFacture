8a25a032c411030ebb78bd9c1a887c2c92aae20d
VKORONA <s.vibert@yahoo.fr>
2026-07-20 18:22:31 +0200

    feat(documents): bind premium estimate fields to approved matrix

--- resources/views/app/pdf/estimate/estimate1.blade.php
+++ resources/views/app/pdf/estimate/estimate1.blade.php
@@ -1,2 +1,22 @@
-@php($autofactureTheme = 'premium')
+@php
+    $autofactureTheme = 'premium';
+    $presentationValues = collect([
+        'project_name' => $estimate->project_name,
+        'project_address' => $estimate->project_address,
+        'purchase_order_number' => $estimate->purchase_order_number,
+        'project_contact' => $estimate->project_contact,
+        'payment_terms' => $estimate->payment_terms_label,
+    ])->filter(static fn ($value) => filled($value));
+
+    if ($presentationValues->isNotEmpty()) {
+        $syntheticFields = $presentationValues->map(
+            static fn ($value, $slug) => (object) [
+                'customField' => (object) ['slug' => $slug],
+                'defaultAnswer' => $value,
+            ]
+        )->values();
+
+        $estimate->setRelation('fields', $estimate->fields->concat($syntheticFields));
+    }
+@endphp
 @include('app.pdf.shared.autofacture-premium-estimate')
