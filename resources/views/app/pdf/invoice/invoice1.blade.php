40de94bad0318962f66c3b2f29d072b600f5728d
VKORONA <s.vibert@yahoo.fr>
2026-07-20 18:22:03 +0200

    feat(documents): bind premium invoice fields to approved matrix

--- resources/views/app/pdf/invoice/invoice1.blade.php
+++ resources/views/app/pdf/invoice/invoice1.blade.php
@@ -1,2 +1,22 @@
-@php($autofactureTheme = 'premium')
+@php
+    $autofactureTheme = 'premium';
+    $presentationValues = collect([
+        'project_name' => $invoice->project_name,
+        'project_address' => $invoice->project_address,
+        'purchase_order_number' => $invoice->purchase_order_number,
+        'project_contact' => $invoice->project_contact,
+        'payment_terms' => $invoice->payment_terms_label,
+    ])->filter(static fn ($value) => filled($value));
+
+    if ($presentationValues->isNotEmpty()) {
+        $syntheticFields = $presentationValues->map(
+            static fn ($value, $slug) => (object) [
+                'customField' => (object) ['slug' => $slug],
+                'defaultAnswer' => $value,
+            ]
+        )->values();
+
+        $invoice->setRelation('fields', $invoice->fields->concat($syntheticFields));
+    }
+@endphp
 @include('app.pdf.shared.autofacture-premium-invoice')
